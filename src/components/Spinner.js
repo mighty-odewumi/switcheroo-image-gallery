91623f3d29bce465cb73a1c58a1f88aea12ba3ed
gr1ntch <odewumimighty@gmail.com>
2023-09-20 05:30:13 +0100

    Fixed spinner position

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,9 +1,17 @@
 import ClipLoader from "react-spinners/ClipLoader";
 
 export default function Spinner() {
+
+  const styles = { 
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  }
+
   return (
-    <div style={{width: "100px", display: "block", margin: "auto"}}>
-      <ClipLoader color="#000" size={50}/>
+    <div style={styles}>
+      <ClipLoader color="#000" size={50} speedMultiplier={.5}/>
     </div>
   )
 }
